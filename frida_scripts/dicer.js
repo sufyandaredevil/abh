370c28ad7d36dfff1519d0c0b0c8941a3ff95522
Ahmed Sufyan <sufyandaredevil@gmail.com>
2024-04-19 12:05:14 +0530

    added frida and related notes

---
+++ frida_scripts/dicer.js
@@ -0,0 +1,18 @@
+//Application to hook: ../apks/Privacyfriendlydicer.apk
+//Script that hook int rollDice(int poolSize, int faceNum), print param values and return
+
+Java.perform(function () {
+  //Get a reference in myDicer of the class "Dicer"
+  var myDicer = Java.use('org.secuso.privacyfriendlydicer.dicer.Dicer');
+
+  myDicer.rollDice.implementation = function (param1, param2) {
+    //Print out param* values
+    send("Parameter 1 = " + param1);
+    send("Parameter 2 = " + param2);
+
+    //calling the original method and return
+    // return this.rollDice(param1, param2);
+    return this.rollDice(4,3);
+  }
+
+});
